cf74b95236f12d7d5ab2e8d5676167b378e5a725
wpoole <wpoole_wen66@163.com>
2020-05-09 10:08:42 +0800

    update production setting

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -4,5 +4,5 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['hellodjango-blog-tutorial-demo.zmrenwu.com']
-HAYSTACK_CONNECTIONS['default']['URL'] = 'http://hellodjango_blog_tutorial_elasticsearch:9200/'
+ALLOWED_HOSTS = ['hellodjango-blog-tutorial-demo.zmrenwu.com', '*']
+HAYSTACK_CONNECTIONS['default']['URL'] = 'http://hellodjango_rest_framework_tutorial_elasticsearch:9200/'
