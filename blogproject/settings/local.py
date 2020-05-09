da3b376907555cb4f92b674f5cd31dddc63b388d
wpoole <wpoole_wen66@163.com>
2020-05-09 10:00:35 +0800

    fix elasticsearch url error

--- blogproject/settings/local.py
+++ blogproject/settings/local.py
@@ -7,4 +7,4 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # 搜索设置
-HAYSTACK_CONNECTIONS['default']['URL'] = 'http://elasticsearch_local:9200/'
+HAYSTACK_CONNECTIONS['default']['URL'] = 'http://hellodjango_rest_framework_tutorial_elasticsearch_local:9200/'
